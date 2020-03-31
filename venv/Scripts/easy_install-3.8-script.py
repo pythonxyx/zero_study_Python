5d9052f870b154e1ea8ec7689c7500f19d1a0efa
pythonxyx <xyxgary@126.com>
2020-03-31 18:43:45 +0800

    Initial commit

---
+++ venv/Scripts/easy_install-3.8-script.py
@@ -0,0 +1,12 @@
+#!E:\徐业鑫\自己所用\Python学习\零基础学Python\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install-3.8'
+__requires__ = 'setuptools==39.1.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==39.1.0', 'console_scripts', 'easy_install-3.8')()
+    )
---
+++ venv/Scripts/pip-script.py
@@ -0,0 +1,12 @@
+#!E:\徐业鑫\自己所用\Python学习\零基础学Python\venv\Scripts\python.exe -x
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip'
+__requires__ = 'pip==10.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip')()
+    )
