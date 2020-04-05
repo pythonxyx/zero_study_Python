40a27ab37a02ff23fcb8119f1f578379d227fcf8
pythonxyx <xyxgary@126.com>
2020-04-05 23:34:00 +0800

    第10章文件操作

---
+++ 第10章  文件及目录操作/antmanor_message.py
@@ -0,0 +1,7 @@
+print('\n','='*10,'蚂蚁庄园动态','='*10)
+# with open('message.txt','w') as file:
+#     file.write('你使用了1张加速卡，小鸡撸起袖子开始双手吃饲料，进食速度大大加快。\n')
+
+with open('message.txt','a') as file:
+    file.write('mingri的小鸡在你的庄园待了22分钟，吃了6g饲料之后，被你赶走了。\n')
+print('\n 写入了一条动态……\n')
